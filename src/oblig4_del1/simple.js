c379d4886145f82db23a167f13350dfde3c036d8
Norgisk <danieldjuvstad@gmail.com>
2025-10-21 18:32:58 +0200

    not_working

---
+++ src/oblig4_del1/simple.js
@@ -0,0 +1,63 @@
+const fornavnInput = document.getElementById("fornavn");
+const etternavnInput = document.getElementById("etternavn");
+const mobilInput = document.getElementById("tlfNummer")
+const password = document.getElementById('password');
+const password_rep = document.getElementById('password_rep');
+const form = document.getElementById('paamelding_form');
+const kjonn = document.getElementsByClassName("kjonn_valg");
+const paameldKnapp = document.getElementById('paameld');
+const paameldt_fornavn = document.getElementById("paameldt_fornavn");
+const paameldt_etternavn = document.getElementById("paameldt_etternavn");
+const paameldt_kjonn = document.getElementById("paameldt_kjonn");
+const paameldt_nr = document.getElementById("paameldt_nr");
+
+
+
+
+function validatePage() {
+
+    form.onsubmit = function (event) {
+        fornavnInput.value = fornavnInput.value.charAt(0).toUpperCase() + fornavnInput.value.slice(1);
+        etternavnInput.value = etternavnInput.value.charAt(0).toUpperCase() + etternavnInput.value.slice(1);
+        if (password_rep.value !== password.value){
+            alert("Passordet er ikke det samme!");
+            event.preventDefault();
+        }
+
+
+
+
+        console.log(fornavnInput.value);
+        console.log(etternavnInput.value);
+        console.log(mobilInput.value);
+        console.log(password.value);
+        console.log(password_rep.value);
+        console.log(kjonn);
+
+
+        localStorage.setItem("fornavn", fornavnInput.value);
+        localStorage.setItem("etternavn", etternavnInput.value);
+        localStorage.setItem("kjonn", kjonn.value);
+        localStorage.setItem("mobil", mobilInput.value);
+
+        window.onload = function() {
+            paameldt_fornavn.innerHTML = fornavnInput.value = localStorage.getItem("fornavn");
+            paameldt_etternavn.innerHTML = etternavnInput.value = localStorage.getItem("etternavn");
+            paameldt_kjonn.innerHTML = localStorage.getItem("kjonn");
+            paameldt_nr.innerHTML = localStorage.getItem("mobil");
+        };
+
+
+         window.location.href = "paameldt.html";
+
+
+
+    }
+
+
+
+
+
+
+}
+validatePage();
